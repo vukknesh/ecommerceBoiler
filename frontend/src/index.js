8cc6c94c4020d034602283bb70e64f52492976c4
Vukknesh <leomcn@hotmail.com>
2019-05-16 23:02:31 -0300

    store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Router } from "react-router-dom";
+import { history } from "./history";
 import App from "./App";
+
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { LastLocationProvider } from "react-router-last-location";
 
 let myComponent = document.getElementById("app");
 if (myComponent !== null) {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router history={history}>
+        <LastLocationProvider>
+          <App />
+        </LastLocationProvider>
+      </Router>
     </Provider>,
     myComponent
   );
